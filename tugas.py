c96a6d6c8438380d94378a30006db1d5374c2ff2
Risky Tri <riskytu78@gmail.com>
2025-07-22 21:45:41 +0700

    bubblesort

--- tugas.py
+++ tugas.py
@@ -1,13 +1,34 @@
 def bubblesort(data):
-    for i in range(len(data)-1,-1,-1):
+    for i in range(len(data)-1,0,-1):
         for j in range(i):
             if data[j] > data[j+1]:
-                tempat = data[j]
-                data[j] = data[j+1]
-                data[j+1] = tempat
-        print(data)          
+                # tempat = data[j]
+                # data[j] = data[j+1]
+                # data[j+1] = tempat
+                data[j], data[j+1] = data[j+1], data[j]
+        print(f"pass {len(data)-i}: {data}")
+        # print(data)   
+        
+# data = [8,5,4,1,2]   
                 
-data = [2,1,4,7,3]
+data = input ("masukan angka: ")
+data = list(map(int, data.split()))
 bubblesort(data)
-print("******")
+print("hasilnya")
 print(data)
+
+def binary_search(array, velue, low, high):
+    if high < low:
+        return -1
+    else:
+        mid = (low + high) // 2
+        
+        if velue > array[mid]: 
+            return binary_search(array, velue, mid +1, high)
+        elif velue < array[mid]:
+            return binary_search(array, velue, low, mid -1)
+        else:
+            return mid
+        
+array = [1,2,3,4,5,6,7,8,9,90] 
+print(binary_search(array, 5, 0, len(array) -1))
